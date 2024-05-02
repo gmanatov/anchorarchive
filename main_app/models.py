9e28e475621d1b8bb8eb7488bb65e140b082e0ec
gmanatov <jfexpr@gmail.com>
2024-05-02 12:19:56 -0500

    little change in how code looks, purely formatting change

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-# class Bookmark(models.Model):
-#     title = models.CharField(max_length=100)
-#     link = models.CharField(max_length=100)
-#     date_created = models.DateField(auto_now_add=True)
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.TextField(max_length=250)
+    date_created = models.DateField(auto_now_add=True)
