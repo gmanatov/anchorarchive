29cfd25e9b06e8a47cfa46fc762c3b1472eb772f
gmanatov <jfexpr@gmail.com>
2024-05-02 12:14:02 -0500

    added some more dummy data, replaced "date" field with "date_created"

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# class Bookmark(models.Model):
+#     title = models.CharField(max_length=100)
+#     link = models.CharField(max_length=100)
+#     date_created = models.DateField(auto_now_add=True)
--- main_app/views.py
+++ main_app/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 # TODO Temporary DUMMY Database - REMOVE THIS AFTER ADDING BOOKMARK MODEL
 bookmarks = [
-    {'date': '04/23/2024', 'title' : 'CSS Anchor Is The Best New CSS Feature Since Flexbox', 'url' : 'https://www.youtube.com/shorts/fO0XD75u2TI'},
-    {'date': '05/02/2024', 'title' : 'Google apprenticeships for 2024 US', 'url' : 'https://www.reddit.com/r/google/comments/18b4tr2/google_apprenticeships_for_2024_us/'},
-    {'date': '04/29/2024', 'title' : 'IBM Apprenticeships', 'url' : 'https://www.ibm.com/careers/search?q=apprentice'},
-    {'date': '05/02/2024', 'title' : 'USA Google Software Apprenticeships', 'url' : 'https://www.google.com/about/careers/applications/jobs/results/?distance=50&hl=en_US&jlo=en_US&location=United%20States&q=%22Software%20Engineering%20(SWE)%20Apprenticeship%22&src=Online%2FGoogle%20Website%2FByF&utm_campaign=&utm_medium=information_technology_apprenticeship_us&utm_source=byf'},
+    {'date_created': '04/23/2024', 'title' : 'CSS Anchor Is The Best New CSS Feature Since Flexbox', 'url' : 'https://www.youtube.com/shorts/fO0XD75u2TI'},
+    {'date_created': '05/02/2024', 'title' : 'Google apprenticeships for 2024 US', 'url' : 'https://www.reddit.com/r/google/comments/18b4tr2/google_apprenticeships_for_2024_us/'},
+    {'date_created': '04/29/2024', 'title' : 'IBM Apprenticeships', 'url' : 'https://www.ibm.com/careers/search?q=apprentice'},
+    {'date_created': '05/02/2024', 'title' : 'USA Google Software Apprenticeships', 'url' : 'https://www.google.com/about/careers/applications/jobs/results/?distance=50&hl=en_US&jlo=en_US&location=United%20States&q=%22Software%20Engineering%20(SWE)%20Apprenticeship%22&src=Online%2FGoogle%20Website%2FByF&utm_campaign=&utm_medium=information_technology_apprenticeship_us&utm_source=byf'},
 ]
 
 def home(request):
