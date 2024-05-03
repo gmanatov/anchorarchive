92094a13ac8afd32d6ad7d24b416bc660fa27fc0
gmanatov <jfexpr@gmail.com>
2024-05-03 12:46:05 -0500

    modified styling, added background video, modified README.md

--- README.md
+++ README.md
@@ -7,8 +7,12 @@ Technologies used:
  - PostgreSQL
  - Materialize CSS
 
-### Getting Started: <link>
+### Getting Started: [<link>](http://127.0.0.1:8000/)
 
 Screenshots:
-![](<screenshot>)
-![](<screenshot>)
+![](<https://i.imgur.com/hfZfRAv.png>)
+![](<https://i.imgur.com/m5PVVvU.png>)
+
+Next Steps:
+ - Add authentication
+ - Deploy
--- main_app/views.py
+++ main_app/views.py
@@ -58,33 +58,3 @@ class TagUpdate(UpdateView):
 class TagDelete(DeleteView):
   model = Tag
   success_url = '/tags'
-
-
-
-
-
-
-
-
-
-
-
-# TODO Temporary DUMMY Database - REMOVE THIS AFTER ADDING BOOKMARK MODEL
-# bookmarks = [
-#     {'id': '3150',
-#      'date_created': '04/23/2024', 
-#      'title' : 'CSS Anchor Is The Best New CSS Feature Since Flexbox', 
-#      'url' : 'https://www.youtube.com/shorts/fO0XD75u2TI'},
-#     {'id': '2677',
-#      'date_created': '05/02/2024', 
-#      'title' : 'Google apprenticeships for 2024 US', 
-#      'url' : 'https://www.reddit.com/r/google/comments/18b4tr2/google_apprenticeships_for_2024_us/'},
-#     {'id': '8142',
-#      'date_created': '04/29/2024', 
-#      'title' : 'IBM Apprenticeships', 
-#      'url' : 'https://www.ibm.com/careers/search?q=apprentice'},
-#     {'id': '4721',
-#      'date_created': '05/02/2024', 
-#      'title' : 'USA Google Software Apprenticeships', 
-#      'url' : 'https://www.google.com/about/careers/applications/jobs/results/?distance=50&hl=en_US&jlo=en_US&location=United%20States&q=%22Software%20Engineering%20(SWE)%20Apprenticeship%22&src=Online%2FGoogle%20Website%2FByF&utm_campaign=&utm_medium=information_technology_apprenticeship_us&utm_source=byf'},
-# ]
