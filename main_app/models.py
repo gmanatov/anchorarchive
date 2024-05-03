0464281c5fd129adf4e666f6d3f07b6ed936bd36
gmanatov <jfexpr@gmail.com>
2024-05-03 05:27:22 -0500

    made sure that after creating new bookmark it redirects to the detail page of added bookmark

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Bookmark(models.Model):
@@ -8,3 +9,7 @@ class Bookmark(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'bookmark_id': self.id})
+    
