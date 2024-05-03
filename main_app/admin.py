7c71a0b5484f2176637668bab02107962b2464ce
gmanatov <jfexpr@gmail.com>
2024-05-03 07:49:35 -0500

    defined many to many (with Bookmark) model Tag and migrated it to database

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Bookmark
+from .models import Bookmark, Tag
 # Register your models here.
-admin.site.register(Bookmark)
+admin.site.register([Bookmark, Tag])
--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,22 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('tags_detail', kwargs={'pk': self.id})
+    
 class Bookmark(models.Model):
     date_created = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=100)
     url = models.TextField(max_length=250)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
